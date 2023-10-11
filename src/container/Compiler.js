d126e3d155d8cc899e66bcc3d0898c5779b677b0
ved-thakre <vedantthakre7@gmail.com>
2023-10-11 08:40:16 +0530

    images added

--- src/container/Compiler.js
+++ src/container/Compiler.js
@@ -48,7 +48,7 @@ const Compiler = () => {
               for="default-range"
               class="inline-block mb-2 pt-2 pr-2 text-lg font-thin text-gray-900 dark:text-white"
             >
-              Font Size
+              <b>Fs</b>
             </label>
             <input
               id="default-range"
@@ -59,9 +59,14 @@ const Compiler = () => {
               }}
               min={1}
               max={35}
-              class="w-24 h-2 ml-1  bg-gray-200 accent-emerald-500 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+              class="w-12 h-2 ml-1  bg-gray-200 accent-emerald-500 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
             ></input>
           </div>
+          {/* <label class="relative inline-flex items-center mb-5 pt2 cursor-pointer">
+            <input type="checkbox" value="" class="sr-only peer"/>
+            <div class="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
+          </label> */}
         </header>
         {/* Compiler */}
         <div className="grid grid-cols-10 w-full">
@@ -77,7 +82,7 @@ const Compiler = () => {
             {/* Wrap the Editor component in a div with rounded-md */}
             <div className="overlay rounded-md overflow-hidden w-full h-full shadow-4xl">
               <Editor
-                height="77vh"
+                height="80vh"
                 language={language?.value || "cpp"}
                 theme="vs-dark"
                 value={code}
